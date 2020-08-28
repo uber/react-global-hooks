a432ee599be28c3ac46d595048f65b8dd729aecb
Diana Suvorova <diana.suvorova@uber.com>
2020-08-28 15:30:12 -0700

    chore: prettier

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 // @flow
 module.exports = {
-  presets: ["@babel/react", "@babel/flow"],
+  presets: ['@babel/react', '@babel/flow'],
   plugins: [
-    "@babel/proposal-class-properties",
-    "@babel/proposal-export-default-from",
-    "version-inline",
+    '@babel/proposal-class-properties',
+    '@babel/proposal-export-default-from',
+    'version-inline',
   ],
   env: {
     test: {
-      presets: ["@babel/env", "@babel/react", "@babel/flow"],
+      presets: ['@babel/env', '@babel/react', '@babel/flow'],
       plugins: [
-        "@babel/transform-runtime",
-        "@babel/proposal-class-properties",
-        "@babel/proposal-export-default-from",
-        "version-inline",
+        '@babel/transform-runtime',
+        '@babel/proposal-class-properties',
+        '@babel/proposal-export-default-from',
+        'version-inline',
       ],
     },
   },
